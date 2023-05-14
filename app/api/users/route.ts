afe0cfd08c4b15c05873b60e47144226e7d81849
nivb52 <nivb52@gmail.com>
2023-05-14 16:18:26 +0300

    NEW

---
+++ app/api/users/route.ts
@@ -0,0 +1,14 @@
+import prisma from '@/lib/prisma';
+import { NextResponse } from 'next/server';
+
+export async function GET(req: Request) {
+    const users = await prisma.user.findMany();
+    
+    return NextResponse.json(users)
+}
+
+
+// export async function POST(req: Request) {
+    
+    
+// }
