87699a24efd60f3a1d9bff23ab25009f18a8a289
nivb52 <nivb52@gmail.com>
2023-05-23 21:25:27 +0300

    FEAT: form to update profile

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,14 +1,40 @@
 import prisma from '@/lib/prisma';
 import { NextResponse } from 'next/server';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../auth/[...nextauth]/route';
 
 export async function GET(req: Request) {
     const users = await prisma.user.findMany();
-    
+
     return NextResponse.json(users)
 }
 
 
-// export async function POST(req: Request) {
-    
-    
-// }
+export async function PUT(req: Request) {
+    //auth
+    const session = await getServerSession(authOptions);
+    const currEmail = session?.user?.email;
+
+    if (!currEmail) {
+        return NextResponse.error();
+    }
+
+    const data: { age: string, bio: string, email: string, name: string, image: string } = await req.json();
+    const age = Number(data.age);
+    const image = data.image.match(/(http(s?):)([\/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)/g);
+    const extractedImage = image?.length ? image[0] : data.image;
+    const user = await prisma.user.update({
+        where: {
+            email: currEmail
+        },
+        data: {
+            age,
+            image: extractedImage,
+            name: data.name,
+            bio: data.bio
+        }
+    });
+
+
+    return NextResponse.json(user);
+}
--- app/dashboard/ProfileForm.tsx
+++ app/dashboard/ProfileForm.tsx
@@ -7,9 +7,28 @@ interface Props {
 }
 export default function ProfilForm({ user }: Props) {
 
+    const updateUser = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault;
+        const fromData = new FormData(e.currentTarget);
+        const body = {
+            name: fromData.get('name'),
+            age: fromData.get('age'),
+            bio: fromData.get('bio'),
+            image: fromData.get('image')
+        }
+        const res = await fetch('/api/users', {
+            method: 'PUT',
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        await res.json();
+    }
+
     return (
         <section className="p-6 dark:bg-gray-800 dark:text-gray-50 bg-slate-50">
-            <form onSubmit={() => console.log('success')} className="container flex flex-col mx-auto space-y-12 ng-untouched ng-pristine ng-valid">
+            <form onSubmit={updateUser} className="container flex flex-col mx-auto space-y-12 ng-untouched ng-pristine ng-valid">
                 <fieldset className="grid grid-cols-4 gap-6 p-6 rounded-md shadow-sm dark:bg-gray-900 bg-slate-200">
                     <div className="space-y-2 col-span-full lg:col-span-1">
                         <p className="font-medium">Profile</p>
@@ -35,7 +54,7 @@ export default function ProfilForm({ user }: Props) {
                         </div>
                         <div className="col-span-full">
                             <div className="flex items-center space-x-2">
-                                <button type="button" className="px-4 py-2 border rounded-md dark:border-gray-100 border-fuchsia-700">Save</button>
+                                <button type="submit" role="button" className="px-4 py-2 border rounded-md dark:border-gray-100 border-fuchsia-700">Save</button>
                             </div>
                         </div>
                     </div>
