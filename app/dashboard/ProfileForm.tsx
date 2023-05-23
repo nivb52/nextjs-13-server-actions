e6c22f9362899b68360fd723eeaf83bb226adb2f
nivb52 <nivb52@gmail.com>
2023-05-23 20:20:48 +0300

    CSS: Form

--- app/dashboard/ProfileForm.tsx
+++ app/dashboard/ProfileForm.tsx
@@ -8,26 +8,40 @@ interface Props {
 export default function ProfilForm({ user }: Props) {
 
     return (
-        <div className="max-w-lg  p-10">
-            <h2 className="text-2xl text-center  underline inset-8 shadow-md" >Edit Your Profile</h2>
-            <form onSubmit={alert} className="flex flex-col pt-10 ">
-                <label htmlFor="name">Name</label>
-                <input type="text" name="name" defaultValue={user?.name ?? ''} />
-                <label htmlFor="bio">Bio</label>
-                <textarea
-                    name="bio"
-                    cols={30}
-                    rows={10}
-                    defaultValue={user?.bio ?? ''}
-                ></textarea>
-                <label htmlFor="age" defaultValue={user?.age ?? 0}>Age</label>
-                <input type="text" name="age" />
-                <label htmlFor="image">Profile Image URL</label>
-                <input type="text" name="image" defaultValue={user?.image ?? ''} />
-
-                <button type="submit" className="bg-green-700 text-white">Save</button>
+        <section className="p-6 dark:bg-gray-800 dark:text-gray-50 bg-slate-50">
+            <form onSubmit={() => console.log('success')} className="container flex flex-col mx-auto space-y-12 ng-untouched ng-pristine ng-valid">
+                <fieldset className="grid grid-cols-4 gap-6 p-6 rounded-md shadow-sm dark:bg-gray-900 bg-slate-200">
+                    <div className="space-y-2 col-span-full lg:col-span-1">
+                        <p className="font-medium">Profile</p>
+                        <p className="text-xs">Edit Your Profile</p>
+                    </div>
+                    <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
+                        <div className="col-span-full sm:col-span-3">
+                            <label htmlFor="name" className="text-sm">Username</label>
+                            <input name="name" type="text" defaultValue={user?.name ?? ''} placeholder="Username" className="underl w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:border-gray-700 dark:text-gray-900" />
+                        </div>
+                        <div className="col-span-full sm:col-span-3">
+                            <label htmlFor="age" className="text-sm">Age</label>
+                            <input name="age" type="text" defaultValue={user?.age ?? 0} placeholder="0" className="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:border-gray-700 dark:text-gray-900" />
+                        </div>
+                        <div className="col-span-full">
+                            <label htmlFor="bio" defaultValue={user?.bio ?? ''} className="text-sm">Bio</label>
+                            <textarea name="bio" placeholder="" cols={30}
+                                rows={10} className="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:border-gray-700 dark:text-gray-900"></textarea>
+                        </div>
+                        <div className="col-span-full sm:col-span-5">
+                            <label htmlFor="image" className="text-sm">Profile Image URL</label>
+                            <input name="image" type="text" defaultValue={user?.image ?? ''} placeholder="https://" className="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:border-gray-700 dark:text-gray-900" />
+                        </div>
+                        <div className="col-span-full">
+                            <div className="flex items-center space-x-2">
+                                <button type="button" className="px-4 py-2 border rounded-md dark:border-gray-100 border-fuchsia-700">Save</button>
+                            </div>
+                        </div>
+                    </div>
+                </fieldset>
             </form>
-        </div>
+        </section>
     )
 
 }
