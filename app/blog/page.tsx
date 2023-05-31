9010738123139902d0d9b9f2af806bc4b8ea032a
nivb52 <nivb52@gmail.com>
2023-05-31 14:00:57 +0300

    FIX: Add Some Text to route

---
+++ app/blog/page.tsx
@@ -0,0 +1,17 @@
+import prisma from '@/lib/prisma';
+
+
+
+export default async function Blog() {
+
+
+    return (
+        <main className=''>
+            <h1 className='text-4xl text-center pt-4'>
+                Blog
+            </h1>
+        </main>
+    )
+
+
+}
